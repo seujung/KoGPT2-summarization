ba2fab5fc2028f2c6df0e06c964f7261adea5213
seujung <digit822gmail.com>
2021-08-26 16:05:56 +0900

    update lightinig version

--- train.py
+++ train.py
@@ -108,7 +108,7 @@ class KoGPTSummaryModule(pl.LightningDataModule):
 class Base(pl.LightningModule):
     def __init__(self, hparams, **kwargs) -> None:
         super(Base, self).__init__()
-        self.hparams = hparams
+        self.save_hyperparameters(hparams)
 
     @staticmethod
     def add_model_specific_args(parent_parser):
